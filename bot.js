e0f8b83187ed6542b552b54f95e76fa76e455f9a
Vakme <4bielak.aleksandra@gmail.com>
2017-09-08 00:39:09 +0200

    Python Zen + better greetings + custom coffee

--- bot.js
+++ bot.js
@@ -1,89 +1,158 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var msg = require('./msg.json');
+var ontime = require('ontime');
+var request = require("request");
+var cheerio = require("cheerio");
+var unsplash = require('unsplash-api');
+
+var helpChannelId = 342377931772919828;
+var malbotId = 352478053877678082;
+var generalChannelId = 342050278964592640;
+var photoArray = [];
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
     token: auth.token,
     autorun: true
 });
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+    unsplashInit();
 });
 
-bot.on("guildMemberAdd", function(member) {
-    logger.info(member.id);
-    bot.sendMessage({
-        to: 342050278964592640,
-        message: 'Witaj, <@' + member.id + '>'
-    });
 
+ontime({
+    cycle: '7:00:00'
+}, function (ot) {
+    packtDownload();
+    ot.done();
+});
+
+function unsplashInit() {
+    unsplash.init(auth.unsplashId);
+    unsplash.searchPhotos('coffee', null, 1, 30, function (error, photos, link) {
+        photoArray = photos;
+        logger.info('Unsplash ready');
+    });
+}
+
+function packtDownload() {
+    request({
+        uri: "https://www.packtpub.com/packt/offers/free-learning"
+    }, function (error, response, body) {
+        logger.info("PacktPub daily info");
+        var $ = cheerio.load(body);
+        var title = $(".dotd-title > h2").text();
+        bot.sendMessage({
+            to: generalChannelId,
+            message: msg.packtMsgBeg + title + msg.packtMsgEnd
+        });
+    });
+}
+
+function greet(id) {
+    logger.info("New Member: " + id);
+    bot.sendMessage({
+        to: generalChannelId,
+        message: '<@' + id + '>' + msg.publicMsg
+    });
+    bot.sendMessage({
+        to: id,
+        message: msg.privateMsg
+    });
+}
+
+function pythonZen(channel) {
+    request({
+        uri: "http://misztal.edu.pl/logs/the-zen-of-python/"
+    }, function (error, response, body) {
+        var $ = cheerio.load(body);
+        var table = [];
+         $("blockquote em").each(function(i, elem) {
+             table[i] = $(this).text();
+         });
+        var rand = Math.floor((Math.random() * table.length));
+        bot.sendMessage({
+            to: channel,
+            message: '*' + table[rand] + '* - The Zen of Python'
+        });
+    });
+}
+
+function coffee(channelID) {
+    var photo = photoArray[Math.floor(Math.random() * photoArray.length)];
+    bot.sendMessage({
+        to: channelID,
+        embed: {
+            title: 'Kawa!',
+            description: 'Oto kawa specjalnie dla ciebie!',
+            url: photo.links.self,
+            image: {
+                url: photo.urls.small
+            },
+            author: {
+                name: photo.user.name,
+                url: photo.user.portfolio_url
+            }
+        }
+    }, function (error, response) {
+        if(error != null)
+            logger.error(error);
+    });
+}
+bot.on("guildMemberAdd", function(member) {
+    greet(member.id);
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.toLowerCase().indexOf('python') !== -1)
-        bot.sendMessage({
-            to: channelID,
-            message: '<@342186363594211356>, aktywacja'
-        });
-
-    //Wiadomości ogólne - ich nie dodajemy
-    if (message.indexOf('Definitywnie?') !== -1)
-        bot.sendMessage({
-            to: channelID,
-            message: 'I ostatecznie!'
-        });
-    else if (message.indexOf('Java') !== -1)
-        bot.sendMessage({
-            to: channelID,
-            message: 'Co za chłam! Tylko malbolge!'
-        });
-    else if (message.indexOf('Malbocie') !== -1) {
-        if (message.indexOf('?') !== -1) {
-            //Do tablicy dopisz odpowiedzi na pytania
-            var answers = ['Oczywiście', 'No co ty!', 'Niewykluczone', 'No', 'Tiaaa...', 'Definitywnie!'];
-            var minimum = 0;
-            var maximum = answers.length;
-            var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+    message = message.toLowerCase();
+    if(channelID != helpChannelId && userID != malbotId) {
+        if (message.indexOf('python') !== -1)
+            pythonZen(channelID);
+        if (message.indexOf('definitywnie') !== -1)
             bot.sendMessage({
                 to: channelID,
-                message: answers[randomnumber]
+                message: 'I ostatecznie!'
             });
-        }
-        else if (message.indexOf(',') !== -1) {
-            var args = message.substring(message.indexOf(',') + 1);
-            logger.info(args);
-            //args = args.splice(1);
+        else if (message.indexOf('malbocie') !== -1) {
+            if (message.indexOf('czy') !== -1) {
+                var answers = ['Oczywiście', 'No co ty!', 'Niewykluczone', 'No', 'Tiaaa...', 'Definitywnie!'];
+                var randomnumber = Math.floor(Math.random() * answers.length);
+                bot.sendMessage({
+                    to: channelID,
+                    message: answers[randomnumber]
+                });
+            }
+            else if (message.indexOf(',') !== -1) {
+                var args = message.substring(message.indexOf(',') + 1);
+                switch (args) {
+                    case ' podaj kawę':
+                        coffee(channelID);
+                        break;
 
-            //case - to teksty po przecinku, umieść je przed default! i Pamiętaj o break!
-            switch (args) {
-                case ' podaj kawę':
-                    bot.sendMessage({
-                        to: channelID,
-                        message: ':coffee:'
-                    });
-                    break;
+                    case ' koduj':
+                        bot.sendMessage({
+                            to: channelID,
+                            message: 'Tylko w malbolge'
+                        });
+                        break;
+                    default:
+                        break;
 
-                case ' koduj':
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'Tylko w malbolge'
-                    });
-                    break;
-                default:
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'Co chcesz?'
-                    });
-
-                // Just add any case commands if you want to..
+                    // Just add any case commands if you want to...
+                }
             }
         }
     }
